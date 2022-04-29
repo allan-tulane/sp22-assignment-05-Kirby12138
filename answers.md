44f66635ead038d6f9391b53b8d60e430299631b
Kirby12138 <82625392+Kirby12138@users.noreply.github.com>
2022-04-28 23:57:41 -0500

    Add files via upload

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Assignment 5
 ## Answers
 
-**Name:**_________________________
+**Name:**_________Dachen Ni________________
 
 
 Place all written answers from `assignment-05.md` here for easier grading.
@@ -11,14 +11,23 @@ Place all written answers from `assignment-05.md` here for easier grading.
 
 
 - **1a.**
+The greedy algorithm would always pick the largest coin available that is smallar than remaining money.
+
+For example, if you got 100 dollars, the greedy algorithm would first pick 64, which is the largest coin less than 100. Then, there are 36 remaining so it will pick 32 as it's the biggest coin less than 36. Finally, there are 4 remaining and the algorithm will pick four. And the result is 3 coins.
 
 
 
+- **1b.**
+The work and span would both be O(logn).
 
 
+- **2a.**
 
+The algorithm doesn't work optimally because the denomiators are arbitrary now.
+An example that it fails will be you got 8 dollar and the available choices are 1, 4, and 5. Optimally you would pick two four dollars, which is 2 coins. However, the greedy algorithm will pick one five dollar and three one dollar, resulting in 4 coins.
 
 - **2b.**
 
+By using memorization to avoiding recomputation, the work would be the number of nodes of DAG, O(n * w). The span would be the longest path in DAG, which is O(n).
 
 
--- main.py
+++ main.py
@@ -14,23 +14,63 @@ def MED(S, T):
         if (S[0] == T[0]):
             return(MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return(1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[1:],T[1:])))
+
 
 
 def fast_MED(S, T, MED={}):
     # TODO -  implement memoization
-    pass
+    dp = [[0] * (len(S)+1) for _ in range(len(T)+1) ]
+    for i in range(len(S)+1): dp[0][i] = i
+    for i in range(len(T)+1): dp[i][0] = i
+    
+    for i in range(1,len(T)+1):
+        for j in range(1, len(S)+1):
+            if S[j-1] == T[i-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+    return dp[-1][-1]
+
+for case in test_cases:
+    print(MED(case[0],case[1]), fast_MED(case[0],case[1]))
 
 def fast_align_MED(S, T, MED={}):
     # TODO - keep track of alignment
-    pass
+    dp = [[0] * (len(S)+1) for _ in range(len(T)+1) ]
+    for i in range(len(S)+1): dp[0][i] = i
+    for i in range(len(T)+1): dp[i][0] = i
+    allign_s = []
+    allign_t = []
+    for i in range(1,len(T)+1):
+        for j in range(1, len(S)+1):
+            if S[j-1] == T[i-1]:
+                dp[i][j] = dp[i-1][j-1]
+                allign_s.append(S[j-1])
+                allign_t.append(S[j-1])
+            else:             
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                mini = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+                if mini == dp[i-1][j]:
+                    allign_s.append('-')
+                    allign_t.append(T[i-1])
+                elif mini == dp[i][j-1]:
+                    allign_s.append(S[i-1])
+                    allign_t.append('-')
+
+
+    return (''.join(allign_s), ''.join(allign_t))
 
 def test_MED():
     for S, T in test_cases:
         assert fast_MED(S, T) == MED(S, T)
-                                 
+
+test_MED()                           
 def test_align():
     for i in range(len(test_cases)):
         S, T = test_cases[i]
         align_S, align_T = fast_align_MED(S, T)
+        print(align_S, align_T)
         assert (align_S == alignments[i][0] and align_T == alignments[i][1])
+
+test_align()
